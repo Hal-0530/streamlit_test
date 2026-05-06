4229f24207f720708feddfe2744b91b6ac7decb1
Hal-0530 <n.tamura0530@gmail.com>
2026-05-07 01:03:44 +0900

    Fix_2

--- my_app/main_app.py
+++ my_app/main_app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 from PIL import Image
 import os
+from pathlib import Path
 
 st.title('メインアプリ')
 st.caption('これはメインアプリです')
 
-image_path = './data/logo.png'
+# main_app.pyから見て data/logo.png を読み込む場合
+current_dir = Path(__file__).parent  # main_app.pyがあるフォルダ
+file_path = current_dir / "data" / "logo.png"
 
-if os.path.exists(image_path):
-    image = Image.open(image_path)
-    st.image(image, caption=f'ロゴ画像:{image_path}', width=200)
+if os.path.exists(file_path):
+    image = Image.open(file_path)
+    st.image(image, caption=f'ロゴ画像:{file_path}', width=200)
 else:
     # どこを探して失敗したか画面に表示させる（デバッグ用）
-    st.error(f"ファイルが見つかりません。検索パス: {image_path}")
+    st.error(f"ファイルが見つかりません。検索パス: {file_path}")
     # 念のため、カレントディレクトリの中身を表示して確認
     st.write(f"現在の作業ディレクトリ: {os.getcwd()}")
