44cb8b8b764696d5eac53a06c4ebd2924bd23dfd
Hal-0530 <n.tamura0530@gmail.com>
2026-05-10 11:51:11 +0900

    202605010 fix-2

--- my_app/pages/page_5.py
+++ my_app/pages/page_5.py
@@ -1,12 +1,53 @@
 import streamlit as st
 
 st.title('データカウント')
- 
-st.write('変数を設定')
-count = 0
- 
-if st.button('push me'):
-    st.write('ボタンを押しました')
-    count += 1
- 
-st.write('count = ', count)
+
+st.subheader('このコードはうまくいかない例')
+
+with st.container():
+    code = '''
+    st.write('ボタンを押してください')
+    count = 0
+    
+    if st.button('push me'):
+        st.write('ボタンを押しました')
+        count += 1
+    
+    st.write('count = ', count)
+    '''
+    st.code(code, language='python')
+
+    st.write('ボタンを押してください')
+    count = 0
+    
+    if st.button('push me'):
+        st.write('ボタンを押しました')
+        count += 1
+    
+    st.write('count = ', count)
+
+st.subheader('以下のコードはうまくいく例')
+
+with st.container():
+    code = '''
+    if 'count' not in st.session_state:
+        st.write('ボタンを押してください')
+        st.session_state['count'] = 0
+    
+    if st.button('push me'):
+        st.write('ボタンを押しました')
+        st.session_state['count'] += 1
+    
+    st.write('count = ', st.session_state['count'])
+    '''
+    st.code(code, language='python')
+
+    if 'count' not in st.session_state:
+        st.write('ボタンを押してください')
+        st.session_state['count'] = 0
+    
+    if st.button('push me'):
+        st.write('ボタンを押しました')
+        st.session_state['count'] += 1
+    
+    st.write('count = ', st.session_state['count'])
